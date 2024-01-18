673526981369acb7633fce5877fc624e0b78a9cb
Daniela <114227863+DanaroNait2002@users.noreply.github.com>
2024-01-18 20:43:44 +0000

    Avances UI
    - Edición de MainMenu Script: El botón de Play activa la selección de nivel de la interfaz

--- Assets/UI/Scripts/MainMenu.cs
+++ Assets/UI/Scripts/MainMenu.cs
@@ -17,8 +17,12 @@ public class MainMenu : MonoBehaviour
     [Header("Windows Tab")]
     [SerializeField] private GameObject windowsTab;
 
+    [Header("Level Selector")]
+    [SerializeField] private GameObject levelSelection;
+
     [Header("Bools")]
-    [SerializeField] private bool startpressed;
+    [SerializeField] private bool startPressed;
+    [SerializeField] private bool playPressed;
 
     #endregion
 
@@ -29,8 +33,10 @@ public class MainMenu : MonoBehaviour
         background.SetActive(true);
         toolbar.SetActive(true);
         windowsTab.SetActive(false);
+        levelSelection.SetActive(false);
 
-        startpressed = false;
+        startPressed = false;
+        playPressed = false;
     }
 
     private void Update()
@@ -42,15 +48,30 @@ public class MainMenu : MonoBehaviour
 
     public void StartButton()
     {
-        if (startpressed)
+        if (startPressed)
         {
             windowsTab.SetActive(false);
-            startpressed = false;
+            startPressed = false;
         }
         else
         {
             windowsTab.SetActive(true);
-            startpressed = true;
+            startPressed = true;
+        }
+    }
+
+    public void PlayButton()
+    {
+        Debug.Log("Pressed");
+        if (playPressed)
+        {
+            levelSelection.SetActive(false);
+            playPressed = false;
+        }
+        else
+        {
+            levelSelection.SetActive(true);
+            playPressed = true;
         }
     }
 
